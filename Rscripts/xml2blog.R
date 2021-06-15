153199efe5f45fca7b04301e9617497fe8e7f05c
Ratnaweera Nils <rata@svma-s-01171.zhaw.ch>
2021-06-16 01:57:42 +0200

    again

--- Rscripts/xml2blog.R
+++ Rscripts/xml2blog.R
@@ -28,7 +28,7 @@ html2markdown <- function(string){
   }
 }
 
-list.files("articles_xml2/",full.names = TRUE) %>%
+canonicals <- list.files("articles_xml2/",full.names = TRUE) %>%
   map_dfr(function(x){
     cont <- x %>%
       xml2::read_xml() %>%
@@ -36,92 +36,150 @@ list.files("articles_xml2/",full.names = TRUE) %>%
 
     canonic <- map_chr(cont, ~get_text(.x, "canonical"))
     tibble(canonic = canonic, x = x)
-  }) %>% View
-
-list.files("articles_xml2/",full.names = TRUE) %>%
-  map(function(y){
-    cont <- y %>%
-      xml2::read_xml() %>%
-      xml2::xml_find_all("content")
-
-    canon <- map_chr(cont, ~get_text(.x, "canonical"))
-    news <- FALSE
-    cont[grepl("^/wissenschaft",canon)] %>%
-      # head(1) %>%
-      map(function(x){
-
-
-        title <- get_text(x, "title")
-
-
-        catid <- get_text(x, "catid")
-        introtext <- get_text(x, "introtext")
-        fulltext <- get_text(x, "fulltext")
-        created <- get_text(x, "created")
-        publish_up <- get_text(x, "publish_up")
-        publish_up <- parse_datetime(publish_up,locale = locale(tz = "CET"))
-        publish_date <- as.Date(publish_up)
-
-        images <- get_text(x, "images")
-        created_by <- get_text(x, "created_by")
-        canonical <- get_text(x, "canonical")
-
-        introtext_md <- html2markdown(introtext) %>%
-          paste(collapse = "\n")
-        fulltext_md <- html2markdown(fulltext) %>%
-          paste(collapse = "\n")
-
-        if(fulltext == ""){fulltext <- introtext}
-
-        canonical2 <- str_replace(canonical, "^/", "_")
-        dirnam <- map(str_split(canonical2, "/"), ~paste(.x[1],.x[2], sep = "-"))
-        # categ <- if(news){""}else{map(str_split(canonical2, "/"), ~.x[2])}
-
-        basenam <- basename(canonical)
-        basenam <- str_sub(basenam, 1, 40)
-        if(news){basenam <- paste(publish_date,basenam, sep = "-")}
-
-        filenam <- paste0(basenam, ".Rmd")
-
-        filename_full <- file.path(dirnam, basenam, filenam)
-
-        file.create2(filename_full)
-        print(filename_full)
-
-        desription <- xml2::read_html(introtext) %>% xml2::xml_text()
-        desription <- str_replace_all(desription, "\n", " ")
-
-        write_lines(
-          glue(
-            "---",
-            "title: {title}",
-            "description: |",
-            "  {desription}",
-            "author: {created_by}",
-            "date: {publish_date}",
-            # ifelse(news,"","categories:"),
-            # ifelse(news,"", "  - {categ}"), # <- this does not work yet!
-            "output:",
-            "  distill::distill_article:",
-            "    self_contained: false",
-            "canonical: {canonical}",
-            "---",
-            "",
-            "",
-            "{fulltext_md}",
-            "",
-            "<!--http://wieselundco.ch{canonical}-->",
-            .sep= "\n"
-          ),
-          file = filename_full,
-          append = FALSE
-        )
-        tryCatch(rmarkdown::render(filename_full), error = function(x){print(paste("failed",filename_full))})
-      }
-      )
-
   })
 
+
+map_chr(str_split(canonicals$canonic, "/"), ~.x[2]) %>%
+  unique() %>%
+  paste0('"',.,'"') %>%
+  cat(sep = ",")
+
+xml2rmd <- function(canonical_keyword, makenews = FALSE){
+
+  stopifnot(canonical_keyword %in% c("14-news","projekt","wissenschaft","2-uncategorised","faszination-kleinraubtiere","mithelfen-anpacken","impressum","","login","ueber-uns","17-partner","10-das-projekt"))
+
+  list.files("articles_xml2/",full.names = TRUE) %>%
+    map(function(y){
+      cont <- y %>%
+        xml2::read_xml() %>%
+        xml2::xml_find_all("content")
+
+      featured <- cont %>%
+        xml2::xml_find_all("featured") %>%
+        xml2::xml_text()
+
+      if(makenews){
+        cont <- cont[featured != 0]
+      }
+
+      canon <- map_chr(cont, ~get_text(.x, "canonical"))
+      mynodes <- cont[grepl(glue("^/{canonical_keyword}"),canon)]
+
+      if(length(mynodes)>0){
+        mynodes %>%
+          # head(1) %>%
+          map(function(x){
+
+
+            title <- get_text(x, "title")
+
+
+            catid <- get_text(x, "catid")
+            introtext <- get_text(x, "introtext")
+            fulltext <- get_text(x, "fulltext")
+            created <- get_text(x, "created")
+            publish_up <- get_text(x, "publish_up")
+            publish_up <- parse_datetime(publish_up,locale = locale(tz = "CET"))
+            publish_date <- as.Date(publish_up)
+
+            images <- get_text(x, "images")%>%
+              jsonlite::fromJSON()
+
+            images <- images[map_lgl(images, ~.x != "")]
+
+            images_chr0 <- paste(imap_chr(images, ~glue("![]({.x}) <!--{.y}-->")), collapse = "\n")
+
+            images_chr <- paste(imap_chr(images, ~glue("{.y}: {.x}")),collapse = "\n")
+
+
+            created_by <- get_text(x, "created_by")
+            canonical <- get_text(x, "canonical")
+
+            introtext_md <- html2markdown(introtext) %>%
+              paste(collapse = "\n")
+            fulltext_md <- html2markdown(fulltext) %>%
+              paste(collapse = "\n")
+
+            if(fulltext == ""){fulltext <- introtext}
+
+            canonical2 <- str_replace(canonical, "^/", "_")
+            dirnam <- case_when(
+              grepl("ueber-uns",canonical_keyword)~map_chr(str_split(canonical2, "/"), ~.x[1]),
+              grepl("1-1-untersuetzung",canonical_keyword)~"projekt-bonus",
+              makenews~"_news",
+              TRUE~map_chr(str_split(canonical2, "/"), ~paste(.x[1],.x[2], sep = "-"))
+            )
+
+            basenam <- basename(canonical)
+            basenam <- str_sub(basenam, 1, 40)
+            if(makenews){
+              basenam <- str_sub(basenam, 1, 30)
+              basenam <- paste(publish_date,basenam, sep = "-")
+            }
+
+            filenam <- paste0(basenam, ".Rmd")
+
+            filename_full <- file.path(dirnam, basenam, filenam)
+
+            file.create2(filename_full)
+            print(filename_full)
+
+            desription <- xml2::read_html(introtext) %>% xml2::xml_text()
+            desription <- str_replace_all(desription, "\n", " ")
+
+            write_lines(
+              glue(
+                "---",
+                "title: '{title}'",
+                "description: |",
+                "  '{desription}'",
+                "author: {created_by}",
+                "date: {publish_date}",
+                images_chr,
+                "output:",
+                "  distill::distill_article:",
+                "    self_contained: false",
+                "canonical: {canonical}",
+                "---",
+                "",
+                images_chr0,
+                "",
+                "{fulltext_md}",
+                "",
+                "<!--http://wieselundco.ch{canonical}-->",
+                .sep= "\n"
+              ),
+              file = filename_full,
+              append = FALSE
+            )
+            tryCatch(rmarkdown::render(filename_full), error = function(x){print(paste("failed",filename_full))})
+          }
+          )
+
+      }
+
+
+    })
+}
+
+
+c(
+  "14-news" = TRUE,
+  "2-uncategorised" = TRUE,
+  "projekt" = FALSE,
+  "wissenschaft" = FALSE,
+  "faszination-kleinraubtiere" = FALSE,
+  "mithelfen-anpacken" = FALSE,
+  # "impressum" = FALSE,
+  # "" = FALSE,
+  # "login" = FALSE,
+  "ueber-uns" = FALSE,
+  "17-partner" = FALSE
+  # "10-das-projekt" = FALSE,
+  ) %>%
+  imap(~ml2rmd(canonical_keyword = .y,makenews = .x))
+
+xml2rmd(canonical_keyword = "2-uncategorised",makenews = TRUE)
 
 
 
